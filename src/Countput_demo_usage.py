cf645d0193d5b84a8168a9b676b6b051bacc5c5b
Erik Pohl <epohl@dmigs.com>
2024-12-30 17:01:05 -0500

    moved source code to src

--- src/Countput_demo_usage.py
+++ src/Countput_demo_usage.py
@@ -4,7 +4,7 @@ Created on Dec 14, 2018
 @author: Erik Pohl
 '''
 
-from Countput import Countput
+from src.Countput import Countput
 word_sentence = 'a b c d e f g h i a c e g i c g'
 word_list = word_sentence.split(' ')
 
